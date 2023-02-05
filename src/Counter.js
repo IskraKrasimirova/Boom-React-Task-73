cb91a291292a20dffe14fb99b0e9e6851e585c11
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2023-02-05 19:28:10 +0200

    Change Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-function Counter() {
-    const [count, setCount] = useState(1);
+function Counter({ initialCount = 1 }) {
+    const [count, setCount] = useState(initialCount);
     useEffect(() => {
         document.title = `Count: ${count}`;
     }, [count]);
 
     return (
-        <>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </>
+        <button onClick={() => setCount(count + 1)}>Count: {count}</button>
     );
 }
 
